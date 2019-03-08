c96fc2a2466ba8f375bd34d013413a0b0494677b
Dustin Langner <dustin.langner1@gmail.com>
2019-03-07 20:41:29 -0800

    Skeleton react app

---
+++ client/src/components/NavigationBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const NavigationBar = ({ shouldShowItems }) => {
+    <nav>
+
+
+
+    </nav>
+}
+
+NavigationBar.propTypes = {
+    shouldShowItems: PropTypes.bool.isRequired
+}
+
+export default NavigationBar
